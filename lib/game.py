102378b2fb549418a8997f87f7f7f880cb7e9d6a
waihins <weixuansun1@gmail.com>
2023-02-24 23:10:51 -0600

    create class frameworks

---
+++ lib/game.py
@@ -0,0 +1,11 @@
+from snake import Snake
+from item import Item
+from map import Map
+
+class Game(Snake, Item, Map): 
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    pass
