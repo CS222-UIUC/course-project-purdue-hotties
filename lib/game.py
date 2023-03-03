fc5dd12624fee7641929d70d45366d95243f234f
David Li <lid1355218215@outlook.com>
2023-03-02 22:40:41 -0600

    finish draw snake

--- lib/game.py
+++ lib/game.py
@@ -11,13 +11,16 @@ NUM_CELLS = 25
 BACKGROUND_COLOR = (163, 214, 28)
 GRASS_COLOR = (158, 207, 31)
 
-class Game(): 
+
+class Game():
     def __init__(self) -> None:
         pygame.init()
-        self.screen = pygame.display.set_mode((NUM_CELLS * SIZE_PER_CELL, NUM_CELLS * SIZE_PER_CELL))
+        self.screen = pygame.display.set_mode(
+            (NUM_CELLS * SIZE_PER_CELL, NUM_CELLS * SIZE_PER_CELL))
         self.screen.fill(BACKGROUND_COLOR)
         self.draw_grass()
-    
+        self.snake = Snake(SIZE_PER_CELL)
+
     def game_loop(self):
         while True:
             for event in pygame.event.get():
@@ -26,16 +29,17 @@ class Game():
                     sys.exit()
                 self.movement(event)
                 pygame.display.update()
+                self.snake.draw_snake(self.screen)
 
-    def movement(self, event): 
+    def movement(self, event):
         if event.type == pygame.KEYDOWN:
             key = event.key
-            if key == pygame.K_UP or key == pygame.K_w: 
+            if key == pygame.K_UP or key == pygame.K_w:
                 print("up")
             if key == pygame.K_RIGHT or key == pygame.K_d:
                 print("right")
             if key == pygame.K_DOWN or key == pygame.K_s:
-                print("down")                
+                print("down")
             if key == pygame.K_LEFT or key == pygame.K_a:
                 print("left")
 
@@ -43,11 +47,13 @@ class Game():
         for row in range(NUM_CELLS):
             for col in range(NUM_CELLS):
                 if row % 2 == 0 and col % 2 == 0:
-                    grass_blk = pygame.Rect(row * SIZE_PER_CELL, col * SIZE_PER_CELL, SIZE_PER_CELL, SIZE_PER_CELL)
+                    grass_blk = pygame.Rect(
+                        row * SIZE_PER_CELL, col * SIZE_PER_CELL, SIZE_PER_CELL, SIZE_PER_CELL)
                     pygame.draw.rect(self.screen, GRASS_COLOR, grass_blk)
                 elif row % 2 != 0 and col % 2 != 0:
-                    grass_blk = pygame.Rect(row * SIZE_PER_CELL, col * SIZE_PER_CELL, SIZE_PER_CELL, SIZE_PER_CELL)
-                    pygame.draw.rect(self.screen, GRASS_COLOR, grass_blk)	
+                    grass_blk = pygame.Rect(
+                        row * SIZE_PER_CELL, col * SIZE_PER_CELL, SIZE_PER_CELL, SIZE_PER_CELL)
+                    pygame.draw.rect(self.screen, GRASS_COLOR, grass_blk)
 
     @staticmethod
     def get_map_size():
