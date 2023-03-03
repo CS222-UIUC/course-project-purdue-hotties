f04412e102e28a9e81d8e536ec376e45709ef50b
waihins <weixuansun1@gmail.com>
2023-03-02 21:18:43 -0600

    implemented item class for single type object

--- lib/item.py
+++ lib/item.py
@@ -1,3 +1,12 @@
+import random
+from pygame.math import Vector2
+
+CELL_NUM = 25
+
+# TODO: create identifiers for different types of items
+
 class Item:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.x = random.randint(0, CELL_NUM - 1)
+        self.y = random.randint(0, CELL_NUM - 1)
+        self.pos = Vector2(self.x, self.y)
