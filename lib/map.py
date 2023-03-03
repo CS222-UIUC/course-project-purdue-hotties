55caa26a42ebec2d50a2af8dcb615c3bb90972b0
YumingxuanGuo <yumingxuanguo@gmail.com>
2023-03-02 22:04:31 -0600

    added member variable 'map_grid' in the map class

--- lib/map.py
+++ lib/map.py
@@ -1,5 +1,6 @@
-import item
+from lib.item import Item
+from pygame.math import Vector2
 
 class Map:
     def __init__(self) -> None:
-        self.map_grid = [[item] * 25 for n in range(25)]
+        self.map_grid = [[Item()] * 25 for n in range(25)]
