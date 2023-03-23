089235d95cac6b25a1def589399c6dc1858803a7
David Li <lid1355218215@outlook.com>
2023-03-23 15:40:01 -0500

    deleted unused class

--- lib/item.py
+++ lib/item.py
@@ -7,6 +7,7 @@ CELL_NUM = 25
 
 # TODO: create identifiers for different types of items
 
+
 class Item:
     def __init__(self, size_per_cell):
         # self.type = "apple" # placeholder for different types
@@ -16,31 +17,14 @@ class Item:
         self.x = random.randint(0, CELL_NUM - 1)
         self.y = random.randint(0, CELL_NUM - 1)
         self.pos = Vector2(self.x, self.y)
-        
-    
+
     def draw_item_util(self):
         x_pos = int(self.x * self.size_per_cell)
         y_pos = int(self.y * self.size_per_cell)
-        item_rect = pygame.Rect(x_pos, y_pos, self.size_per_cell, self.size_per_cell)
+        item_rect = pygame.Rect(
+            x_pos, y_pos, self.size_per_cell, self.size_per_cell)
         return (self.apple, item_rect)
 
     def draw_item(self, screen):
         asset, loc = self.draw_item_util()
         screen.blit(asset, loc)
-        
-    def item_type(self) -> str:
-        return "item"
-
-class Apple(Item):
-    def __init__(self, size_per_cell):
-        super().__init__(size_per_cell)
-
-    def item_type(self) -> str:
-        return "apple"
-    
-class Posion(Item):
-    def __init__(self, size_per_cell):
-        super().__init__(size_per_cell)
-    
-    def item_type(self) -> str:
-        return "posion"
