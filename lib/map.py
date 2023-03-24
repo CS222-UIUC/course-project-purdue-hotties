c454c466f6ad0f9288d1613b88aa28a31c70eef0
Mayojet <yujiem4@illinois.edu>
2023-03-23 21:58:12 -0500

    random

--- lib/map.py
+++ lib/map.py
@@ -1,12 +1,16 @@
 from lib.item import Item
 from lib.snake import Snake
 from pygame.math import Vector2
-
+import random
+CELL_NUM = 25
 class Map:
     def __init__(self) -> None:
         self.map_grid = [[Item()] * 25 for n in range(25)]
-        self.snake = Snake(self.surface, 2)
+
         # TODO: put snake in a random grid
+        self.x = random.randint(0, CELL_NUM - 1)
+        self.y = random.randint(0, CELL_NUM - 1)
+        self.snake = Snake(self.x, self.y)
          
 
     def tick(self):
